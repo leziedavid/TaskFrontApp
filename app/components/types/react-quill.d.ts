9923091a86f1cc69d9383ba3a4bf16d6903b456c
leziedavid <lezie01.tra@gmail.com>
2024-10-29 11:32:10 +0100

    QuillEditorProps modifier2

--- app/components/types/react-quill.d.ts
+++ app/components/types/react-quill.d.ts
@@ -1,14 +1,14 @@
-declare module 'react-quill' {
-    import { Component } from 'react';
+// declare module 'react-quill' {
+//     import { Component } from 'react';
 
-    export interface ReactQuillProps {
-        value?: string;
-        onChange?: (value: string) => void;
-    }
+//     export interface ReactQuillProps {
+//         value?: string;
+//         onChange?: (value: string) => void;
+//     }
 
-    export default class ReactQuill extends Component<ReactQuillProps> {
-        focus() {
-            throw new Error('Method not implemented.');
-        }
-}
-}
+//     export default class ReactQuill extends Component<ReactQuillProps> {
+//         focus() {
+//             throw new Error('Method not implemented.');
+//         }
+// }
+// }
